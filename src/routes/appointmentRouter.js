be46487287cb4e7456ea67e33f60b6a54bfcfe78
bayu firnanda <bafirgenius@gmail.com>
2021-02-18 17:31:00 +0700

    revisi create and validation appointment

--- src/routes/appointmentRouter.js
+++ src/routes/appointmentRouter.js
@@ -2,10 +2,6 @@ const router = require('express').Router()
 const appointmentMiddleware = require('../middlewares/appointmentMiddleware')
 const appointmentController = require('../controllers/appointmentController')
 
-router.get("/appointment", appointmentController.browse)
-
-router.get("/appointment/:id", appointmentController.read)
-
 router.post("/appointment/:userId",appointmentMiddleware.validateBody, appointmentController.create)
 
 module.exports = router
