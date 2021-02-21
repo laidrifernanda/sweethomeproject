129e0ed31de9e8baccbac5ee61631da2e535b644
bayu firnanda <bafirgenius@gmail.com>
2021-02-21 18:27:08 +0700

    resolve again

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@
 const authRoutes = require("./auth");
 const userAuthRoutes = require("./userAuth")
 const appointmentRoutes = require('./appointment')
+const updateUserRouter = require("./updateUser")
 
 
 //Module exports
 module.exports = {
   authRoutes,
   userAuthRoutes,
-  appointmentRoutes
+  appointmentRoutes,
+  updateUserRouter
 };
--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ const {
   showcaseRoutes,
   dashboardRoutes,
 } = require("./admin/routes");
-const { authRoutes, userAuthRoutes, appointmentRoutes } = require("./routes");
+const { authRoutes, userAuthRoutes, appointmentRoutes,  updateUserRouter } = require("./routes");
 
 //Import data
 const { PATH_ADMIN, PATH_USER } = process.env;
@@ -80,7 +80,7 @@ app.use(PATH_ADMIN, dashboardRoutes);
 app.use(PATH_USER, authRoutes);
 app.use(PATH_USER, userAuthRoutes);
 app.use(PATH_USER, appointmentRoutes);
-app.use(PATH_USER,updateUserRoutes)
+app.use(PATH_USER, updateUserRouter)
 
 //Module Exports
 module.exports = app;
