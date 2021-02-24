576ac628b783083b907e5f3776672444bb76cf0f
Leokrita Jessica <leokritajessica@gmail.com>
2021-02-24 15:06:28 +0700

    add get api showcase all data user

--- src/routes/showcase.js
+++ src/routes/showcase.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const showcaseController = require("../controllers/showcase")
 
+router.get("/showcase", showcaseController.browse )
 router.get("/showcase/project", showcaseController.project )
-router.get("/showcase/portofolio",showcaseController.profile)
+router.get("/showcase/portofolio", showcaseController.profile)
+
 module.exports = router
