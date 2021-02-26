e7eb2a003b5fca374a0bc5978b5ff5faeb2de391
Leokrita Jessica <leokritajessica@gmail.com>
2021-02-26 19:23:22 +0700

    fix payment user

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -3,31 +3,31 @@ const paymentService = require("../services/payment");
 
 //Module exports
 module.exports = {
-  find: async (req, res) => {
-    const {paymentId} = req.params
-    try {
-      const paymentData = await paymentService.find(paymentId);
-      res
-      .status(200)
-      .send({message: "Success get data", data: paymentData});
-    } catch (err) {
-      res.status(400).json({error: err.message})
-    }
-  },
   upload: async (req, res) => {
-    const { body, file, user } = req;
-    const {projectId} = req.params;
-    const uploadFile = file.location;
-    const newPayment = {
-      ...body,
-      receipt: uploadFile,
-      user: user._id
-    }
+    const { file } = req;
+    
     try {
-      const uploadPayment = await paymentService.upload(newPayment, projectId);
+      const uploadFile = file.location;
       res
         .status(200)
-        .send({ message: "upload receipt success", data: uploadPayment });
+        .send({ message: "upload receipt success", data: uploadFile });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  },
+  payment: async (req, res) => {
+    const { body, user } = req;
+    const { projectId } = req.params;
+
+    //update user
+    const newPayment = {
+      ...body,
+      project: projectId,
+      user: user._id,
+    };
+    try {
+      const savePayment = await paymentService.payment(newPayment, projectId);
+      res.send({ status: 200, message: savePayment });
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
--- src/services/payment.js
+++ src/services/payment.js
@@ -3,13 +3,28 @@ const { paymentModel, projectModel } = require("../model");
 
 //Module exports
 module.exports = {
-  find: async (paymentId) => {
-    return await paymentModel.findById({_id: paymentId});
+  findAll: async (page, limit) => {
+    return await paymentModel
+      .find()
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec();
   },
-  upload: async (newPayment, projectId) => {
-    const paymentData = new paymentModel(newPayment)
-    await projectModel.findByIdAndUpdate({_id: projectId}, {status: "On Going"})
-    await paymentData.save()
-    return paymentData
+  find: async (paymentId) => {
+    return await paymentModel.findById({ _id: paymentId });
+  },
+  payment: async (newPayment, projectId) => {
+    const paymentData = new paymentModel(newPayment);
+    await projectModel.findByIdAndUpdate(
+      { _id: projectId },
+      { status: "On Going" }
+    );
+    return await paymentData.save();
+  },
+  getPagination: async (page, limit) => {
+    const totalItem = await paymentModel.countDocuments();
+    const activePage = page;
+    const totalPage = Math.ceil(totalItem / limit);
+    return { totalItem, activePage, totalPage };
   },
 };
