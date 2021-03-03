888b5b657f403a603458610f21c4186e35242610
Leokrita Jessica <leokritajessica@gmail.com>
2021-03-03 13:45:11 +0700

    fix calendar admin to show data timeslot

--- src/admin/services/calendar.js
+++ src/admin/services/calendar.js
@@ -1,15 +1,19 @@
 //Import model
 const { appointmentModel } = require("../../model");
 const moment = require("moment");
+const timeslot = require("../../model/timeslot");
 
 //Module exports
 module.exports = {
   find: async (year, month) => {
-    let date = new Date(`${year}-${month}-01`)
-    date = moment(date)
+    let date = new Date(`${year}-${month}-01`);
+    date = moment(date);
     return await appointmentModel
-      .find({date: {$gte: date.toDate(), $lte: date.endOf('month').toDate()}})
-      .populate({path: "user", select: ["firstname","lastname"]})
-      .select(["date"])
-  }
+      .find({
+        date: { $gte: date.toDate(), $lte: date.endOf("month").toDate() },
+      })
+      .populate({ path: "user", select: ["firstname", "lastname"] })
+      .populate({ path: "timeslot" , select: ["start","end"]})
+      .select(["date"]);
+  },
 };
